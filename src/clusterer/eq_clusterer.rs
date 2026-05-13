19f2a3f181f6b969ea0c96c600e08c8c8007a10f
Alexei Dmitrievtsev <a.dmitrievtsev@yadro.com>
2026-05-13 17:42:26 +0300

    remove unused imports

--- src/clusterer/eq_clusterer.rs
+++ src/clusterer/eq_clusterer.rs
@@ -41,59 +41,56 @@ impl<Hash: SBCHash + std::fmt::Debug> Clusterer<Hash> for EqClusterer {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use crate::{decoder, encoder, hasher, SBCMap, SBCScrubber};
-    use chunkfs::chunkers::{SizeParams, SuperChunker};
-    use chunkfs::hashers::Sha256Hasher;
-    use chunkfs::FileSystem;
-
-    fn generate_test_data() -> Vec<u8> {
-        const TEST_DATA_SIZE: usize = 16000;
-        (0..TEST_DATA_SIZE).map(|_| rand::random::<u8>()).collect()
-    }
 
     // TODO: correct metrics for SBC with features
+
     /*
-    #[test]
-    fn scrub_should_return_correct_scrub_measurements_for_eq_clusterer() {
-        let test_data = generate_test_data();
-        let chunk_size = SizeParams::new(2 * 1024, 8 * 1024, 16 * 1024);
+        fn generate_test_data() -> Vec<u8> {
+            const TEST_DATA_SIZE: usize = 16000;
+            (0..TEST_DATA_SIZE).map(|_| rand::random::<u8>()).collect()
+        }
 
-        let mut fs = FileSystem::new_with_scrubber(
-            HashMap::default(),
-            SBCMap::new(decoder::GdeltaDecoder::new(false)),
-            Box::new(SBCScrubber::new(
-                hasher::AronovichHasher,
-                EqClusterer,
-                encoder::GdeltaEncoder::new(false),
-            )),
-            Sha256Hasher::default(),
-        );
 
-        let mut handle = fs
-            .create_file("file".to_string(), SuperChunker::new(chunk_size))
-            .unwrap();
-        fs.write_to_file(&mut handle, &test_data).unwrap();
-        fs.close_file(handle).unwrap();
+        #[test]
+        fn scrub_should_return_correct_scrub_measurements_for_eq_clusterer() {
+            let test_data = generate_test_data();
+            let chunk_size = SizeParams::new(2 * 1024, 8 * 1024, 16 * 1024);
 
-        let scrub_report = fs.scrub().unwrap();
+            let mut fs = FileSystem::new_with_scrubber(
+                HashMap::default(),
+                SBCMap::new(decoder::GdeltaDecoder::new(false)),
+                Box::new(SBCScrubber::new(
+                    hasher::AronovichHasher,
+                    EqClusterer,
+                    encoder::GdeltaEncoder::new(false),
+                )),
+                Sha256Hasher::default(),
+            );
 
-        let cluster_report = &scrub_report.clusterization_report;
-        assert!(cluster_report.total_cluster_size > 0);
-        assert!(cluster_report.number_of_clusters > 0);
-        assert!(cluster_report
-            .number_of_vertices_in_cluster
-            .values()
-            .all(|&v| v == 1));
-        assert!(cluster_report.distance_to_vertices_in_cluster.is_empty());
-        assert!(cluster_report
-            .distance_to_other_clusters
-            .values()
-            .all(|v| !v.is_empty()));
-        assert!(cluster_report
-            .cluster_dedup_ratio
-            .values()
-            .all(|&v| v == 0.0));
-    }
-     */
+            let mut handle = fs
+                .create_file("file".to_string(), SuperChunker::new(chunk_size))
+                .unwrap();
+            fs.write_to_file(&mut handle, &test_data).unwrap();
+            fs.close_file(handle).unwrap();
+
+            let scrub_report = fs.scrub().unwrap();
+
+            let cluster_report = &scrub_report.clusterization_report;
+            assert!(cluster_report.total_cluster_size > 0);
+            assert!(cluster_report.number_of_clusters > 0);
+            assert!(cluster_report
+                .number_of_vertices_in_cluster
+                .values()
+                .all(|&v| v == 1));
+            assert!(cluster_report.distance_to_vertices_in_cluster.is_empty());
+            assert!(cluster_report
+                .distance_to_other_clusters
+                .values()
+                .all(|v| !v.is_empty()));
+            assert!(cluster_report
+                .cluster_dedup_ratio
+                .values()
+                .all(|&v| v == 0.0));
+        }
+         */
 }
